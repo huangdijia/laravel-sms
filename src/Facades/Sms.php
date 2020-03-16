79eb3df4fa1d9b88684bca36d3c2367bc8e811d6
huangdijia <huangdijia@gmail.com>
2020-03-16 11:50:07 +0800

    update phpdoc

--- src/Facades/Sms.php
+++ src/Facades/Sms.php
@@ -5,21 +5,21 @@ namespace Huangdijia\Sms\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method public staitc __construct($app)
- * @method public staitc register(string $name, Closure $callback)
- * @method public staitc \Huangdijia\Sms\Sms use(string $name = null)
- * @method public staitc \Huangdijia\Sms\Sms driver(string $name = null)
- * @method public staitc \Huangdijia\Sms\Sms[] drivers()
- * @method public staitc array getCustomDrivers()
- * @method public staitc \Huangdijia\Sms\Sms resolve($name)
- * @method public staitc \Huangdijia\Sms\Contracts\Driver  createDriver(array $config)
- * @method public staitc array getConfig(string $name)
- * @method public staitc string getDefaultDriver()
- * @method public static self to(string $to)
- * @method public static self content(string $content)
- * @method public static self withRules(array $rules, array $messages)
- * @method public static \Huangdijia\Sms\Response send()
- * @method public static array info()
+ * @method staitc void __construct($app)
+ * @method staitc void register(string $name, Closure $callback)
+ * @method staitc \Huangdijia\Sms\Sms use(string $name = null)
+ * @method staitc \Huangdijia\Sms\Sms driver(string $name = null)
+ * @method staitc \Huangdijia\Sms\Sms[] drivers()
+ * @method staitc array getCustomDrivers()
+ * @method staitc \Huangdijia\Sms\Sms resolve($name)
+ * @method staitc \Huangdijia\Sms\Contracts\Driver  createDriver(array $config)
+ * @method staitc array getConfig(string $name)
+ * @method staitc string getDefaultDriver()
+ * @method static static to(string $to)
+ * @method static static content(string $content)
+ * @method static static withRules(array $rules, array $messages)
+ * @method static \Huangdijia\Sms\Response send()
+ * @method static array info()
  * @package Huangdijia\Sms
  */
 class Sms extends Facade
